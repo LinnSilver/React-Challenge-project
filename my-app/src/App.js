f04494d6bf52cedddcabc5e8e533396a521ca28a
LinnSilver <baralinn@gmail.com>
2023-05-31 14:57:12 +0000

    Add Loader

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,18 +3,20 @@ import css from './App.module.css';
 // import NavBarForm from "./components/NavBarForm";
 // import NavBarSimple from "./components/NavBarSimple";
 // import PostItem from "./components/PostItem";
-import NavBarChild from "./components/NavBarChild";
+// import NavBarChild from "./components/NavBarChild";
+import Loader from "./components/Loader";
 
 //  <Sidebar />
 //  <NavBarForm />
 //  <NavBarSimple />
 //  <PostItem />
+//  <NavBarChild />
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarChild />
+      <Loader />
       
     </div>
   );
