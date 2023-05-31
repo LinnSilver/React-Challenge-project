2ac1992d6752e634850356046acfd3131373f13d
LinnSilver <baralinn@gmail.com>
2023-05-31 14:52:15 +0000

    Add Post item

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,19 @@
 import css from './App.module.css';
 // import Sidebar from "./components/Sidebar";
-import NavBarSimple from "./components/NavBarSimple";
 // import NavBarForm from "./components/NavBarForm";
+// import NavBarSimple from "./components/NavBarSimple";
+import PostItem from "./components/PostItem";
 
-// <Sidebar />
-//   <NavBarForm />
+//  <Sidebar />
+//  <NavBarForm />
+//  <NavBarSimple />
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarSimple />
-    
+      <PostItem />
+      
     </div>
   );
 }
