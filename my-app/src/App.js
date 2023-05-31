a5aa4cce3e35b514902cf68a765fb529033db5dc
LinnSilver <baralinn@gmail.com>
2023-05-31 14:54:56 +0000

    Add NavBarChild

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,19 @@ import css from './App.module.css';
 // import Sidebar from "./components/Sidebar";
 // import NavBarForm from "./components/NavBarForm";
 // import NavBarSimple from "./components/NavBarSimple";
-import PostItem from "./components/PostItem";
+// import PostItem from "./components/PostItem";
+import NavBarChild from "./components/NavBarChild";
 
 //  <Sidebar />
 //  <NavBarForm />
 //  <NavBarSimple />
+//  <PostItem />
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <PostItem />
+      <NavBarChild />
       
     </div>
   );
