769cd3183da8409578657eac652cb3c2f012a278
LinnSilver <baralinn@gmail.com>
2023-06-04 12:32:38 +0000

    Update app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,18 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
-import NavBarSimple from "./components/NavBarSimple";
-
-import NavBarChild from "./components/NavBarChild";
 import Content from "./components/Content";
-import Loader from "./components/Loader";
 import ContentHooks from "./components/ContentHooks";
-// import PostItem from "./components/PostItem";
-// <PostItem />
 
 function App() {
   return (
     <div className={css.App}>
-     <Sidebar />
-     <NavBarForm />
-     <NavBarSimple />
-     
-     <NavBarChild />
-     <Content />
-     <ContentHooks />
-     <Loader />
-      
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+      {/* <Content /> */}
+      <ContentHooks />
     </div>
   );
 }
